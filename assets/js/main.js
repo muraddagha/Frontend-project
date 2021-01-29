57e82e4c229795914c2ce5d982b3b0fc7edd0a8b
muraddagha <muradha@code.edu.az>
2021-01-29 14:47:56 +0400

    Modal Login and Sign Up added

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,17 @@ $(document).ready(function () {
   $(".mobile-dropdown").click(function () {
     $(this).find(".mobile-dropdown-content").slideToggle();
   });
+
+  $(".signup-btn").click(function () {
+    $(".login-tab").css("display", "none");
+    $(".signup-tab").fadeIn("fast");
+    $(".login-btn").css("background-color", "white").css("color", "#000");
+    $(this).css("background-color", "#0000fe").css("color", "white");
+  });
+  $(".login-btn").click(function () {
+    $(".signup-tab").css("display", "none");
+    $(".login-tab").fadeIn("fast");
+    $(".signup-btn").css("background-color", "white").css("color", "#000");
+    $(this).css("background-color", "#0000fe").css("color", "white");
+  });
 });
